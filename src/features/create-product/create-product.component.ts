f3a425c9c7afefab987ae38d6e60031063ad3631
Sheila <sheilarreis@gmail.com>
2025-08-24 15:39:44 -0300

    feat(create-product): create product form

--- src/features/create-product/create-product.component.ts
+++ src/features/create-product/create-product.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators,  } from '@angular/forms';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatInputModule  } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent {
+  form = new FormGroup({
+    name: new FormControl<string>('',{
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+  });
 
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.form.controls.name.value;
+    }
+  }
 }
