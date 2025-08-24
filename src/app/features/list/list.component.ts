2cf0b86adcbbc764a85bc2b3fb050f256098feb8
Sheila <sheilarreis@gmail.com>
2025-08-24 14:20:54 -0300

    refactor(product-list): create listing component

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { CardComponent } from "../../../features/list/components/card/card.component";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
---
+++ src/features/list/components/card/card.component.ts
@@ -0,0 +1,27 @@
+import { Component, computed, Input, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Product } from '../../../../app/shared/interfaces/product.interface';
+
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input({ required: true }) product!: Product;
+
+  productName = computed(() => this.product.name);
+  productDescription = computed(() => this.product.description);
+  productPrice = computed(() => this.product.price);
+  productId = computed(() => this.product.id);
+
+  ngOnInit(): void {
+    if (!this.product) {
+      throw new Error('CardComponent: required @Input() "product" is missing. Bind a Product in the parent component.');
+    }
+  }
+}
