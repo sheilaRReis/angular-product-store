84ee3008ebf2fb4cde4e6bc5bce56311c8901cab
Sheila <sheilarreis@gmail.com>
2025-08-24 16:34:02 -0300

    style(product-create): adjust layout and styling for product creation component

--- src/features/create-product/create-product.component.ts
+++ src/features/create-product/create-product.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators,  } from '@angular/forms';
 import { MatFormFieldModule  } from '@angular/material/form-field';
 import { MatInputModule  } from '@angular/material/input';
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButton],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
@@ -14,13 +15,13 @@ export class CreateProductComponent {
   form = new FormGroup({
     name: new FormControl<string>('',{
       nonNullable: true,
-      validators: [Validators.required]
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
     }),
   });
 
   onSubmit(): void {
     if (this.form.valid) {
-      this.form.controls.name.value;
+       this.form.controls.name.value;
     }
   }
 }
