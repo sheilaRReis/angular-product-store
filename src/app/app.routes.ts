ae55f94632b2c041e4061dce022dca130418c4e6
Sheila <sheilarreis@gmail.com>
2025-08-24 15:16:45 -0300

    refactor(create-product): lazy loading component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,10 @@ import { ListComponent } from './features/list/list.component';
 export const routes: Routes = [{
     path: '',
     component: ListComponent,
+},
+{
+    path: 'create-product',
+    loadComponent: () => 
+      import("../features/create-product/create-product.component").then(m => m.CreateProductComponent)  
+    
 }];
